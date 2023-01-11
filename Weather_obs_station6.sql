e79e68a9f76cda08c1c862b040a9e28e13bf1f52
Raghav Mohan <120266773+Raghav-Mohan06@users.noreply.github.com>
2023-01-11 23:25:00 +0530

    Add files via upload

---
+++ Weather_obs_station6.sql
@@ -0,0 +1,5 @@
+select DISTINCT CITY from STATION where city like 'A%';
+select DISTINCT CITY from STATION where city like 'E%'; 
+select DISTINCT CITY from STATION where city like 'I%';
+select DISTINCT CITY from STATION where city like 'O%';
+select DISTINCT CITY from STATION where city like 'U%';
